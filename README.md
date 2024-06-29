9fd28f2b5c67619c7cb4d1d26222666f2b520ab3
Nobuchika Tanaka <codequokka@gmail.com>
2024-06-30 07:36:48 +0900

    Install hop.nvim

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ```console
 sh -c "$(curl -fsLS get.chezmoi.io)" -- -b "${HOME}/.local/bin" -d init --apply codequokka
 ```
+
+```console
+chezmoi apply -S . --verbose
+```
--- private_dot_config/nvim/lua/mappings.lua
+++ private_dot_config/nvim/lua/mappings.lua
@@ -7,4 +7,7 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+-- hop-nvim
+map("n", "<leader>Hc", "<cmd>HopChar2<CR>", { desc = "hop-nvim HopChar2" })
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
