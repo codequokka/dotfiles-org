afc0a1f144f9de6dda138e53de0406224d98f49d
Nobuchika Tanaka <codequokka@gmail.com>
2024-07-21 00:21:40 +0000

    Use ubuntu:24.04 image

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ email="codequokka@gmail.com"
 
 cd "$(dirname "$0")" || exit
 sh -c "$(curl -fsLS chezmoi.io/get)" -- -b "${HOME}/.local/bin"
-chezmoi init --promptString email="${email}" -S .
-chezmoi apply -S .
+"${HOME}/.local/bin/chezmoi" init --promptString email="${email}" -S .
+"${HOME}/.local/bin/chezmoi" apply -S .
