6830523bc692601d6e4e757458cdfbcdd3d0212c
Nobuchika Tanaka <codequokka@gmail.com>
2024-08-09 22:26:54 +0000

    Use template for name

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+name="Nobuchika Tanaka"
 email="codequokka@gmail.com"
 
 cd "$(dirname "$0")" || exit
 sh -c "$(curl -fsLS chezmoi.io/get)" -- -b "${HOME}/.local/bin"
-"${HOME}/.local/bin/chezmoi" init --promptString email="${email}" -S .
+"${HOME}/.local/bin/chezmoi" init \
+    --promptString name="${name}" \
+    --promptString email="${email}" -S .
 "${HOME}/.local/bin/chezmoi" apply -S .
